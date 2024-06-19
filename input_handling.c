6c2fa4262a472c35578c1b3d084842a790ada08b
Mr-Don-Leo <maksimababayan@gmail.com>
2024-06-19 15:25:20 +0400

    Finalized parsing

--- input_handling.c
+++ input_handling.c
@@ -6,7 +6,7 @@
 /*   By: mbabayan <mbabayan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 13:11:39 by mbabayan          #+#    #+#             */
-/*   Updated: 2024/06/18 18:47:27 by mbabayan         ###   ########.fr       */
+/*   Updated: 2024/06/19 15:14:19 by mbabayan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char	*join_strings(int argc, char **argv)
 	
 	index = 1;
 	if (argv[1] == NULL || argv[1][0] == '\0')
-		exiting(NULL, NULL, "Error\nEmpty input.");
+		exiting(NULL, NULL, "Error\n");
 	str = ft_strdup(argv[1]);
 	while (++index <= argc)
 	{
 		if (check_space(argv[index]))
-			exiting(str, NULL, "Error\nInvalid input.");
+			exiting(str, NULL, "Error\n");
 		str = ft_strjoin(str, argv[index]);
 	}
 	return (str);
@@ -38,17 +38,17 @@ t_input	process_input(int argc, char **argv)
 	p_values.split_values = ft_split(p_values.value_string, ' ');
 	if (!p_values.split_values)
 		exiting(p_values.value_string, NULL,
-			"Error\nFailed to split values.");
+			"Error\n");
 	p_values.total_values = count_values(p_values.split_values);
 	p_values.index = 0;
 	free(p_values.value_string);
 	if (p_values.split_values[p_values.index] == 0
 		|| p_values.split_values[p_values.index] == NULL)
-		exiting(NULL, p_values.split_values, "Error\nEmpty input.");
+		exiting(NULL, p_values.split_values, "Error\n");
 	p_values.array = parse_array(p_values.split_values, p_values.total_values, &p_values);
 	if (!p_values.array)
 		exiting(NULL, NULL,
-			"Error\nFailed to parse values into INT array.");
+			"Error\n");
 	return (p_values);
 }
 
